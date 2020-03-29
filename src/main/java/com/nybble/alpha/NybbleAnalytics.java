d05d7aa1bbff6a102f2b1b6a3fbcef81ad29b348
NybbleHub <lehuede.s@gmail.com>
2020-03-29 10:45:22 -0700

    Development of MatchAggregation and Elasticsearch sink configuration to send events to ES instance.

--- src/main/java/com/nybble/alpha/NybbleAnalytics.java
+++ src/main/java/com/nybble/alpha/NybbleAnalytics.java
@@ -10,15 +10,27 @@ import com.nybble.alpha.event_stream.EventStreamTrigger;
 import com.nybble.alpha.event_stream.EventWindowFunction;
 import com.nybble.alpha.event_stream.MultipleEventProcess;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.elasticsearch.ElasticsearchSinkFunction;
+import org.apache.flink.streaming.connectors.elasticsearch.RequestIndexer;
+import org.apache.flink.streaming.connectors.elasticsearch6.ElasticsearchSink;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
-import java.util.Properties;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.Requests;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
 
 
 public class NybbleAnalytics {
@@ -37,20 +49,55 @@ public class NybbleAnalytics {
 		FlinkKafkaConsumer<ObjectNode> windowsLogsConsumer = new FlinkKafkaConsumer("windows-logs", logsSchema, kafkaProperties);
 		windowsLogsConsumer.setStartFromEarliest();
 
+		// Set up ElasticSearch environment
+		List<HttpHost> httpHosts = new ArrayList<>();
+		httpHosts.add(new HttpHost("127.0.0.1", 9200, "http"));
+
+		// Create a ElasticSearch sink where index is "events" to store events from Kafka.
+		ElasticsearchSink.Builder<String> esSinkDataBuilder = new ElasticsearchSink.Builder<>(httpHosts, new ElasticsearchSinkFunction<String>() {
+			public IndexRequest createIndexRequest(String element) throws IOException {
+				ObjectMapper mapper = new ObjectMapper();
+				HashMap eventNode = mapper.readValue(element, HashMap.class);
+
+				return Requests.indexRequest()
+						.index("events")
+						.type("_doc")
+						.source(eventNode);
+			}
+
+			@Override
+			public void process(String element, RuntimeContext ctx, RequestIndexer indexer) {
+				try {
+					indexer.add(createIndexRequest(element));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// Configuration for the bulk requests; this instructs the sink to emit after every element, otherwise they would be buffered
+		esSinkDataBuilder.setBulkFlushMaxActions(1);
+
 		// Create Control Event (Sigma converted rules) Stream
 		DataStream<ObjectNode> sigmaRuleSourceStream = env.addSource(new SigmaSourceFunction())
 				.process(new MultipleRulesProcess())
 				.keyBy(new ControlDynamicKey());
 		sigmaRuleSourceStream.print();
 
-		// Create Security Event (From Kafka) Stream
-		DataStream<ObjectNode> windowsLogsStream = env.addSource(windowsLogsConsumer)
+		// Create Security Event Stream (From Kafka Stream)
+		DataStream<ObjectNode> securityEventsStream = env.addSource(windowsLogsConsumer)
 				.map(new MapFunction<ObjectNode, ObjectNode>() {
 					@Override
 					public ObjectNode map(ObjectNode eventNodes) throws Exception {
 						return eventNodes.get("value").deepCopy();
 					}
-				})
+				});
+
+		// Send Events to Elasticsearch
+		securityEventsStream.map(ObjectNode::toString).addSink(esSinkDataBuilder.build());
+
+		// Create Security Event (From Flink Stream) Stream and process for rule match.
+		DataStream<ObjectNode> ruleEngineStream = securityEventsStream
 				.process(new MultipleEventProcess())
 				.keyBy(new EventDynamicKey())
 				.timeWindow(Time.days(1))
@@ -61,7 +108,7 @@ public class NybbleAnalytics {
 
 		// Create a Sigma Alert Stream containing events filtered from rules.
 		DataStream<ObjectNode> alertStream = sigmaRuleSourceStream
-				.connect(windowsLogsStream)
+				.connect(ruleEngineStream)
 				.flatMap(new LogSourceMatcher())
 				.flatMap(new ControlEventMatcher())
 				.flatMap(new MatchAggregation())
--- src/main/java/com/nybble/alpha/alert_engine/MatchAggregation.java
+++ src/main/java/com/nybble/alpha/alert_engine/MatchAggregation.java
@@ -1,6 +1,7 @@
 package com.nybble.alpha.alert_engine;
 
 import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -17,15 +18,6 @@ public class MatchAggregation implements FlatMapFunction<Tuple2<ObjectNode, Obje
     private HashMap<ObjectNode, Tuple2<Date, Integer>> fieldCountMap;
     private HashMap<ObjectNode, Tuple2<Date, Integer>> fieldByGroupCountMap;
     private ObjectMapper jsonMapper = new ObjectMapper();
-    private Configuration jsonPathConf;
-
-    /*public MatchAggregation(Configuration jsonPathAggregationConf) {
-        // Create JsonPath configuration to search value of fields.
-        Configuration jsonPathConfig = Configuration.defaultConfiguration()
-                .addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL)
-                .addOptions(Option.ALWAYS_RETURN_LIST)
-                .addOptions(Option.SUPPRESS_EXCEPTIONS);
-    }*/
 
     @Override
     public void flatMap(Tuple2<ObjectNode, ObjectNode> controlEventMatch, Collector<Tuple2<ObjectNode, ObjectNode>> collector) throws Exception {
@@ -40,6 +32,12 @@ public class MatchAggregation implements FlatMapFunction<Tuple2<ObjectNode, Obje
         } else if (!controlEventMatch.f1.get("rule").get(0).get("aggregation").isNull()
                 && !controlEventMatch.f1.get("rule").get(0).get("timeframe").isNull()) {
 
+
+            // Create JsonPath configuration to search value of fields in EventNodes.
+            Configuration jsonPathConfig = Configuration.defaultConfiguration()
+                    .addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL)
+                    .addOptions(Option.SUPPRESS_EXCEPTIONS);
+
             // Get aggregation node from rule
             ObjectNode aggregationNode = controlEventMatch.f1.get("rule").get(0).get("aggregation").deepCopy();
 
@@ -47,10 +45,64 @@ public class MatchAggregation implements FlatMapFunction<Tuple2<ObjectNode, Obje
 
             if (aggregationNode.has("aggfield") && aggregationNode.has("groupfield")) {
 
+                // Create fieldByGroupCountNode
+                ObjectNode fieldByGroupCountNode = jsonMapper.createObjectNode();
+
+                // Get value of aggfield from EventNode
+                String aggfield = JsonPath.using(jsonPathConfig)
+                        .parse(jsonMapper.writeValueAsString(controlEventMatch.f0))
+                        .read("$." + aggregationNode.get("aggfield"));
+
+                // Get value of groupfield from EventNode
+                String groupfield = JsonPath.using(jsonPathConfig)
+                        .parse(jsonMapper.writeValueAsString(controlEventMatch.f0))
+                        .read("$." + aggregationNode.get("groupfield"));
+
+                if (aggfield != null && groupfield != null) {
+                    // Add values in fieldByGroupCountNode. This Node is the fieldByGroupCountMap HashMap key.
+                    fieldByGroupCountNode.put("ruleid", controlEventMatch.f1.get("ruleid").asText());
+                    fieldByGroupCountNode.put("aggfield", aggfield);
+                    fieldByGroupCountNode.put("groupfield", groupfield);
+
+                    System.out.println("Field by group count node is : " + fieldByGroupCountNode);
+
+                } else {
+                    System.out.println("\"aggfield\" : \"" + aggregationNode.get("aggfield").asText() +
+                            "\" or \"group-field\" : \"" + aggregationNode.get("groupfield").asText() +
+                            "\" has not been found in event. Please check rule and corresponding events.");
+                }
+
             } else if (aggregationNode.has("aggfield") && !aggregationNode.has("groupfield")) {
+
+                // Create fieldCountNode
+                ObjectNode fieldCountNode = jsonMapper.createObjectNode();
+
+                // Get value of aggfield from EventNode
+                String aggfield = JsonPath.using(jsonPathConfig)
+                        .parse(jsonMapper.writeValueAsString(controlEventMatch.f0))
+                        .read("$." + aggregationNode.get("aggfield"));
+
+                if (aggfield != null) {
+                    // Add values in fieldCountNode. This Node is the fieldCountMap HashMap key.
+                    fieldCountNode.put("ruleid", controlEventMatch.f1.get("ruleid").asText());
+                    fieldCountNode.put("aggfield", aggfield);
+
+                    System.out.println("Field  count node is : " + fieldCountNode);
+
+                } else {
+                    System.out.println("\"aggfield\" : \"" + aggregationNode.get("aggfield").asText() +
+                            "\" has not been found in event. Please check rule and corresponding events.");
+                }
 
             } else if (!aggregationNode.has("aggfield") && !aggregationNode.has("groupfield")) {
 
+                // Create globalCountNode
+                ObjectNode globalCountNode = jsonMapper.createObjectNode();
+
+                // Add values in globalCountNode. This Node is the globalCountMap HashMap key.
+                globalCountNode.put("ruleid", controlEventMatch.f1.get("ruleid").asText());
+
+                System.out.println("Global count node is : " + globalCountNode);
             }
         }
     }
